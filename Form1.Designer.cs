d116b314b875a964797c78d963a0f61e17f068d6
Werther Maia <wertherm@gmail.com>
2020-02-05 01:10:40 -0200

    Commit Inicial

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace TimeSheet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblData = new System.Windows.Forms.Label();
+            this.lblHora = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ckbUltimaAtividade = new System.Windows.Forms.CheckBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 49);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(268, 154);
+            this.textBox1.TabIndex = 2;
+            // 
+            // lblData
+            // 
+            this.lblData.AutoSize = true;
+            this.lblData.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblData.Location = new System.Drawing.Point(12, 24);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(52, 22);
+            this.lblData.TabIndex = 0;
+            this.lblData.Text = "Data";
+            // 
+            // lblHora
+            // 
+            this.lblHora.AutoSize = true;
+            this.lblHora.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHora.Location = new System.Drawing.Point(227, 24);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(53, 22);
+            this.lblHora.TabIndex = 1;
+            this.lblHora.Text = "Hora";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(292, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(68, 20);
+            this.toolStripMenuItem1.Text = "Opcoes...";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(93, 22);
+            this.toolStripMenuItem2.Text = "Sair";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // ckbUltimaAtividade
+            // 
+            this.ckbUltimaAtividade.AutoSize = true;
+            this.ckbUltimaAtividade.Location = new System.Drawing.Point(12, 209);
+            this.ckbUltimaAtividade.Name = "ckbUltimaAtividade";
+            this.ckbUltimaAtividade.Size = new System.Drawing.Size(102, 17);
+            this.ckbUltimaAtividade.TabIndex = 5;
+            this.ckbUltimaAtividade.Text = "Última Atividade";
+            this.ckbUltimaAtividade.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 238);
+            this.Controls.Add(this.ckbUltimaAtividade);
+            this.Controls.Add(this.lblHora);
+            this.Controls.Add(this.lblData);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Time Sheet";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            this.Resize += new System.EventHandler(this.TrayMinimizerForm_Resize);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblData;
+        private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.CheckBox ckbUltimaAtividade;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using SysTimer = System.Timers;
+
+//Evento Control.Resize
+//    http://msdn.microsoft.com/pt-br/library/system.windows.forms.control.resize(v=vs.110).aspx
+//LifeCycle
+//    http://msdn.microsoft.com/en-us/library/86faxx0d(v=vs.110).aspx
+//    http://www.c-sharpcorner.com/uploadfile/mamta_m/windows-forms-events-lifecycle
+//    http://stackoverflow.com/questions/1575508/in-what-order-do-net-windows-forms-events-fire
+
+namespace TimeSheet
+{
+    /// <summary>
+    /// A hora que você chegar, você executa o App, ele inicia no Tray, guardando a hora que foi iniciado.
+    /// Quando ele completar uma hora de execução ele mostra o Form solicitando a descrição da atividade e continua
+    /// com o mesmo processo até que o CheckBox "Última Atividade" seja checkado, assim após uma hora de execução
+    /// ele sincroniza todas as atividades com a planilha e se fecha automaticamente.
+    /// OBS: Este App levou 1 dia (8 horas) para ser desenvolvido, com duas pesquisas:
+    ///     Como para manipular o Timer
+    ///     Como minimizar Apps no System Tray
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        SysTimer.Timer timerDinamico = new SysTimer.Timer();
+        bool flagCarregamentoSecundario = false;
+        int intervalo = 15000;
+
+        //Para ocultar completamente o Form da Task Bar tem que desabilitar a propriedade ShowInTaskBar,
+        //senão com a propriedade WindowsState ele ficará somente minimizado! Só que sempre que o form
+        //for minimizado ele ficará oculto da Task Bar!
+        public Form1()
+        {
+            InitializeComponent();
+            AtribuirLabels();
+            CriarMenuNotify();
+        }
+
+        void AtribuirLabels()
+        {
+            lblData.Text = DateTime.Now.ToShortDateString();
+            lblHora.Text = DateTime.Now.ToShortTimeString();
+        }
+
+        void IniciarTimer(int intervaloTimer)
+        {
+            timerDinamico.Interval = intervaloTimer;
+            timerDinamico.Start();
+        }
+
+        void PararTimer(int intervaloTimer)
+        {
+            timerDinamico.Interval = intervaloTimer;
+            timerDinamico.Enabled = false;
+            timerDinamico.Stop();
+        }
+
+        void CriarMenuNotify()
+        {
+            MenuItem configurarMenuItem = new MenuItem("Configuração");
+            MenuItem sairMenuItem = new MenuItem("Sair", new EventHandler(exitToolStripMenuItem_Click));
+            notifyIcon1.ContextMenu = new ContextMenu(new MenuItem[] { configurarMenuItem, sairMenuItem });
+        }
+
+        void MostrarForm()
+        {
+            //flagCarregamentoSecundario = true;
+            this.ShowInTaskbar = true;
+            this.WindowState = FormWindowState.Normal;
+            //this.Visible = true;
+            this.Activate();
+            //this.Show();
+        }
+
+        void EventoElapsed(object sender, SysTimer.ElapsedEventArgs e)
+        {
+            //lblResultado.Text = DateTime.Now.ToString("o");
+            //MessageBox.Show("Informe a descrição da atividade!", "Definir Atividade", MessageBoxButtons.OK, MessageBoxIcon.Question);
+
+            //Se usar this.Show() neste ponto vai ocorrer o erro:
+            //  Cross-thread operation not valid: Control accessed from a thread other than the thread it was created on
+            //Portanto é necessário fazer o tratamento abaixo:
+            this.Invoke(new MethodInvoker(MostrarForm));
+            //http://stackoverflow.com/questions/142003/cross-thread-operation-not-valid-control-accessed-from-a-thread-other-than-the
+        }
+
+        #region System Tray
+        //http://www.codeproject.com/Articles/27599/Minimize-window-to-system-tray
+        //http://www.codeproject.com/Articles/18683/Creating-a-Tasktray-Application
+        //http://stackoverflow.com/questions/7625421/minimize-app-to-system-tray
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        /// <summary>
+        /// Toda hora que ocorre o evento "Resize" do Form este método é executado!
+        ///     Quando o Form é minimizado!
+        ///     Application.Run(new Form1());
+        ///     this.ShowInTaskbar = true; - O método TrayMinimizerForm_Resize() é executado duas vezes!
+        ///     this.WindowState = FormWindowState.Normal;
+        ///     MessageBox.Show();
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TrayMinimizerForm_Resize(object sender, EventArgs e)
+        {
+            //notifyIcon1.Visible = true;
+            notifyIcon1.BalloonTipTitle = "Time Sheet minimizado no Tray App!";
+            notifyIcon1.BalloonTipText = "A cada uma hora será solicitada a descrição da atividade.";
+            notifyIcon1.ShowBalloonTip(500);
+
+            //Se o Form foi minimizado e o Timer ainda não foi iniciado...
+            if (this.WindowState == FormWindowState.Minimized && timerDinamico.Interval == 100)
+            {
+                //http://stackoverflow.com/questions/1329900/net-event-every-minute-on-the-minute-is-a-timer-the-best-option
+                timerDinamico.AutoReset = false;
+                timerDinamico.Elapsed += new SysTimer.ElapsedEventHandler(EventoElapsed);
+
+                IniciarTimer(intervalo);
+            }
+            else if (this.WindowState == FormWindowState.Normal)
+                PararTimer(100);
+            //else if (this.WindowState == FormWindowState.Minimized)
+            //    this.ShowInTaskbar = false;
+
+            //if (!flagCarregamentoSecundario)
+            //{
+            //    if (FormWindowState.Minimized == this.WindowState)
+            //    {
+            //        notifyIcon1.Visible = true;
+            //        notifyIcon1.ShowBalloonTip(500);
+            //        this.Hide();
+            //    }
+            //    else if (FormWindowState.Normal == this.WindowState)
+            //    {
+            //        notifyIcon1.Visible = false;
+            //    }
+            //}
+        }
+        #endregion
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = false;
+        }
+    }
+}
